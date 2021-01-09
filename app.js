3791904428614fca34e6a638ca25941b390dfa26
Zhernovkov Maxim <max.zhernovkov@gmail.com>
2021-01-09 14:43:28 -0500

    Test message to debug Heroku env configs

--- app.js
+++ app.js
@@ -557,5 +557,7 @@ app.view('modal_submission', async ({ ack, body, view }) => {
 // Main -> start the Bolt app.
 (async () => {
   await app.start(process.env.PORT);
-  logger.info('KarrotAwards Bolt app started!');
+  logger.info('KarrotAwards -> ⚡️ Bolt app started!');
+
+  logger.info(process.env.MONGODB_USER_NAME);
 })();
